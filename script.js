ca9345c573e620210dec5fcaa5a9c39fd3cee606
Golden-Exp <pranavkrishnan01@gmail.com>
2024-12-01 11:43:27 +0000

    uploaded the working code

---
+++ script.js
@@ -0,0 +1,28 @@
+const container = document.querySelector("#container");
+let number = 16;
+const size = document.querySelector("#button");
+function createGrid(size) {
+    container.innerHTML = ""
+    for (let i = 0; i < size*size; i++) {
+        const square = document.createElement("div");
+        square.classList.add("one");    
+        square.style.width = `${550/size}px`
+        square.style.height = `${550/size}px`
+        square.addEventListener("mouseover", () => {
+            square.style.background = getRandomColor()
+        })
+        container.appendChild(square);
+    }
+}
+function getRandomColor(){
+    let r = Math.random() * 256
+    let g = Math.random() * 256
+    let b = Math.random() * 256
+    return `rgb(${r}, ${g}, ${b})`
+}
+size.addEventListener("click", () => {
+    let number = prompt("Enter the size from 1 - 100");
+    createGrid(number)
+})
+
+createGrid(16);
